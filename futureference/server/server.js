c4288e7f57d9017bf30e560463a293b9b2d389b7
Jen Lamere <jennielamere@gmail.com>
2013-03-22 22:02:34 -0400

    more quotes

--- futureference/server/server.js
+++ futureference/server/server.js
@@ -10,6 +10,12 @@ Meteor.publish("quotes", function() {
 
 Meteor.startup(function() {
 	//just add a quote if the db is empty for testing purposes
-	if (Quotes.find().count() == 0)
-			Quotes.insert({quote: "hi, test", owner: 343, likes: 1 });
+	if (Quotes.find().count() == 0){
+			Quotes.insert({quote: "hi, test", owner: 432, likes: 1 });
+			Quotes.insert({quote: "clementine club", owner: 343, likes: 14 });
+			Quotes.insert({quote: "whaddup", owner: 6533, likes: 11 });
+			Quotes.insert({quote: "Do you like chinese food? I dont", owner: 5344, likes: 1 });
+			Quotes.insert({quote: "No Cops No Stops", owner: 543, likes: 0 });
+		}
+
 });
