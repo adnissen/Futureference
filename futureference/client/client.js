6427e3dd950dfeab5773cf1b98811b6a6459ed0c
Andrew Nissen <andrew_nissen@yahoo.com>
2013-03-22 20:16:03 -0400

    added a basic user page that lists all the public quotes

--- futureference/client/client.js
+++ futureference/client/client.js
@@ -5,10 +5,8 @@ Template.main.greeting = function () {
  return "Welcome to futureference.";
 };
 
-Template.main.events({
-  'click input' : function () {
-  // template data, if any, is available in 'this'
-  if (typeof console !== 'undefined')
-    console.log("You pressed the button");
-  }
-  });
+Template.main.userQuotes = function() {
+ return Quotes.find();
+};
+
+Template.main.events({});
