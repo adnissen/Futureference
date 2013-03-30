b4c920a185f8b659e2cc952e56202d738cabe288
Andrew Nissen <andrew_nissen@yahoo.com>
2013-03-30 14:45:04 -0400

    set a bunch of session variables to 0

--- futureference/client/client.js
+++ futureference/client/client.js
@@ -2,6 +2,11 @@ Meteor.subscribe("directory");
 Meteor.subscribe("quotes");
 
 Session.setDefault("currentPage", 0);
+Session.setDefault("requests", 0);
+Session.setDefault("favs", 0);
+Session.setDefault("pageUsername", 0);
+Session.setDefault("isFriend", 0);
+Session.setDefault("fSent", 0);
 
 Accounts.ui.config({
 	passwordSignupFields: 'USERNAME_AND_EMAIL'
